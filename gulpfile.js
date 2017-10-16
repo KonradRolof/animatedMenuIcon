77db871fc141cdd2f644ec4d1ae0b4c977100e7a
Konrad Rolof <konrad.rolof@gmail.com>
2017-10-16 22:57:32 +0200

    [TASK] add browserSync, remove bower dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
-var autoprefixer = require('gulp-autoprefixer'),
-    sequence = require('run-sequence'),
-    cssnano = require('gulp-cssnano'),
-    rename = require('gulp-rename'),
-    rimraf = require('rimraf'),
-    gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    es = require('event-stream');
+/**
+ * load plugins
+ */
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require("browser-sync").create();
+var sourcemaps = require('gulp-sourcemaps');
+var sequence = require('run-sequence');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var rimraf = require('rimraf');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var es = require('event-stream');
 
 // Browsers to target when prefixing CSS.
 var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
 
-
+/**
+ * define tasks
+ */
 // Delete the "dist" folder
 // This happens every time a build starts
 gulp.task('clean', function (done) {
@@ -22,6 +29,7 @@ gulp.task('clean', function (done) {
 // In production, the CSS is compressed
 gulp.task('sass', function () {
     var demo = gulp.src('./src/scss/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compact'
         }).on('error', sass.logError))
@@ -29,6 +37,7 @@ gulp.task('sass', function () {
             browsers: COMPATIBILITY
         }))
         .pipe(cssnano())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./demo/css'));
 
     var normal = gulp.src('./src/scss/burger-icon.scss')
@@ -57,18 +66,28 @@ gulp.task('sass', function () {
 });
 
 gulp.task('copy-normalize', function () {
-    gulp.src('./bower_components/normalize-css/normalize.css')
+    gulp.src('./node_modules/normalize-css/normalize.css')
         .pipe(gulp.dest('./demo/css'));
 });
 
 gulp.task('copy-jQuery', function () {
-    gulp.src('./bower_components/jquery/dist/jquery.slim.min.js')
+    gulp.src('./node_modules/jquery/dist/jquery.slim.min.js')
         .pipe(gulp.dest('./demo/js'));
 });
 
 gulp.task('copyfiles', function () {
     gulp.src('./src/**/*.html')
         .pipe(gulp.dest('./demo'));
+});
+
+gulp.task('serve', ['build'], function() {
+
+    browserSync.init({
+        server: './demo'
+    });
+
+    gulp.watch('src/scss/*.scss', ['sass']);
+    gulp.watch('src/index.html', ['copyfiles']).on('change', browserSync.reload);
 });
 
 // Build the "dist" folder by running all of the above tasks
